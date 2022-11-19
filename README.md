fda7546d2df52cc8b01b5f65c9587ecba4fe5e96
Da Ke <da_ke@outlook.com>
2022-11-19 21:06:06 +0800

    Include conan: re-orgnize project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Logger Module
+
+Create conan recipe if not created
+`conan new logger/1.0 --template-cmake_lib`
+
+Build and install to local cache
+`conan create . -pr:b macos_x86_release`
---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+
+class LoggerConan(ConanFile):
+    name = "logger"
+    version = "1.0"
+
+    # Optional metadata
+    license = "MIT"
+    author = "keda"
+    url = "https://github.com/Drake072/conan-demo-logger.git"
+    description = "A simple logger for demo purpose"
+    topics = ("demo", "logger")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["logger"]
